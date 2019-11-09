e5e0f8f245b24d4e9da3ddf61ebdfdc93485251a
Ghanna <stavka1000@gmail.com>
2019-11-09 14:29:39 +0300

    Game

--- lesson5/script.js
+++ lesson5/script.js
@@ -1,10 +1,31 @@
-
-function removeByJquery(){
+function removeByJquery() {
     var element = $("#words-to-delete");
     element.remove();
 }
 
-function add(){
+function add() {
     var but2 = $("#add-but");
     but2.after("<h1>Hello, world!</h1>");
+}
+var current = "blue";
+let colors = ["none","none","none","none","none","none","none","none","none"]
+
+function turn(number) {
+   
+
+    $("#s" + number).css("background-color", current);
+
+    colors[number-1]=current;
+    
+    if (current === "blue") {
+        current = "red";
+    } else {
+        current = "blue";
+    }
+
+    $("#s" + number).prop("disabled", true);
+
+   
+    
+
 }
